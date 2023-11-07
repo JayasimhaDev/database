0404b8607006165284818e1e955fdae5b08a43a3
JayasimhaDev <jayawebx01@gmail.com>
2023-11-07 20:39:09 +0530

    update

--- Index.js
+++ Index.js
@@ -1,61 +1,60 @@
 const express = require("express");
-const bodyParser=require('body-parser');
+const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
-const cors=require('cors')
+const cors = require("cors");
 
-const port=process.env.PORT ||1890;
+const port = process.env.PORT || 1890;
 const app = express();
-app.use(express.json())
+app.use(express.json());
 app.use(cors());
 //hello
-mongoose.connect(
-    "mongodb+srv://jayasimha:jaya18jaya@cluster0.j1q0dzg.mongodb.net/Movieapp", 
+mongoose
+  .connect(
+    "mongodb+srv://jayawebx01:13n3c24018@cluster0.kxhgjnf.mongodb.net/Movieapp",
     {
-        useNewUrlParser: true,
-        useUnifiedTopology: true
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
     }
-).then(()=>{
-    console.log("connection successfull");
-
-})
+  )
+  .then(() => {
+    console.log(" Mongodb connection successfully");
+  });
 const userschema = new mongoose.Schema({
-  email:String,
-  password:String,
-  name:String
+  email: String,
+  password: String,
+  name: String,
 });
-const User = mongoose.model('User', userschema);
-app.post('/register', async (req,res) => {
-  console.log(await req.body)
-  let user= await new  User(req.body);
-  user.save((err,response)=>{
-if(err){
-  res.send({message:'error while adding item'})
-}else{
-  res.send({message:"User added successfully"})
-}
-  });
+const User = mongoose.model("User", userschema);
 
-})
-app.post('/login', async (req,res) => {
-  console.log(await req.body)
-  let {email,password}= req.body;
-  
-  User.find({email:email},(err,response)=>{
-if(err){
-  res.send({message:'error while adding item'})
-}else{
-  if(response.length>0){
-    if(response[0].password===password){
-      res.send(response)
-    }else{
-      res.send({message:'password didt match'})
+app.post("/register", async (req, res) => {
+  console.log(await req.body);
+  let user = await new User(req.body);
+  user.save((err, response) => {
+    if (err) {
+      res.send({ message: "error while adding item" });
+    } else {
+      res.send({ message: "User added successfully" });
     }
-  }else{
-    res.send({message:'user not registered'})
-  }
-
-}
   });
+});
 
-})
-app.listen(port, () => console.log("Server is running"))
+app.post("/login", async (req, res) => {
+  let { email, password } = req.body;
+
+  User.find({ email: email }, (err, response) => {
+    if (err) {
+      res.send({ message: "error while adding item" });
+    } else {
+      if (response.length > 0) {
+        if (response[0].password === password) {
+          res.send(response);
+        } else {
+          res.send({ message: "password did't match" });
+        }
+      } else {
+        res.send({ message: "user not registered" });
+      }
+    }
+  });
+});
+app.listen(port, () => console.log("Server is running"));
